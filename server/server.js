806466f8a68ef8d1db82f7b664fc939003d12ce8
João Martins <jmbaltazarmartins@gmail.com>
2024-04-23 22:54:40 +0100

    .

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
-require('dotenv').config();
+require('dotenv').config({ path: '../db_credentials.env' });
 
 // Create an Express application
 const app = express();
